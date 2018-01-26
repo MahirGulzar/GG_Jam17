48feb2751c92d05a2ae7719da05ae2944ca78659
Mahir Gulzar <mahirgulzar@gmail.com>
2018-01-26 21:20:29 +0200

    Main Menu and Gameplay Scene Structure

---
+++ Assets/1-Game_Scripts/Gameplay/EventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventHandler : MonoBehaviour {
+
+    public void OnPause()
+    {
+        // Pause funcitionality
+        Debug.Log("Pause funcitionality");
+    }
+
+
+    public void OnMainMenu()
+    {
+        // Main Menu funcitionality
+        Debug.Log("Main Menu funcitionality");
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
---
+++ Assets/1-Game_Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+
+    public void OnPlay()
+    {
+        // play funcitionality
+        Debug.Log("play funcitionality");
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+    public void OnQuit()
+    {
+        // Quit funcitionality
+        Debug.Log("quit funcitionality");
+    }
+
+
+    public void OnOptions()
+    {
+        // Options funcitionality
+        Debug.Log("options funcitionality");
+    }
+}
